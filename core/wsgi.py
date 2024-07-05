ff6d710126a4014ef735e14fb55f445747ccfcd8
Vedant <vedantkumbhar973@gmail.com>
2024-07-05 14:19:25 +0530

    wsgi

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+ 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/home/ubuntu/OptipackDjango')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
